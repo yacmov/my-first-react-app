9f1b77596af056d6af87d93693e71f7d323a26e3
Cedric Ko <yacmov@gmail.com>
2025-07-03 00:34:33 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+const App = () => {
+  return (
+    <main>
+      <div className="pattern"/>
+      <div className="wrapper">
+        <header>
+          <h1>Find <span className="text-gradient">Movie</span> You'll Enjoy Without the Hassle</h1>
+        </header>
+
+        <p>Search</p>
+      </div>
+    </main>
+  );
+};
+
+export default App;
